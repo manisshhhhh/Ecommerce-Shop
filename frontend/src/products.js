0105008180760b67f4f24e0df7477d1446891493
Manish <2002mybaran@gmail.com>
2023-06-02 08:58:02 +0530

    frontend Added

---
+++ frontend/src/products.js
@@ -0,0 +1,129 @@
+const products = [
+    {
+        _id: '1',
+        name: 'Iphone 14 pro',
+        image: '/images/phone.jpg',
+        description: 'Description for Product 5',
+        brand: 'Brand 1',
+        category: 'Category 1',
+        price: 24.99,
+        countInStock: 1,
+        rating: 4.5,
+        numReviews: 15
+    },
+
+    {
+        _id: '2',
+        name: 'Cannon EOS',
+        image: '/images/camera.jpg',
+        description: 'Description for Product 3',
+        brand: 'Brand 3',
+        category: 'Category 1',
+        price: 14.99,
+        countInStock: 8,
+        rating: 4.2,
+        numReviews: 12
+    },
+    {
+        _id: '3',
+        name: 'G-3 Gaming Mouse',
+        image: '/images/mouse.jpg',
+        description: 'Description for Product 4',
+        brand: 'Brand 2',
+        category: 'Category 2',
+        price: 9.99,
+        countInStock: 3,
+        rating: 4.0,
+        numReviews: 7
+    },
+    {
+        _id: '4',
+        name: 'Sony theator 4 pro',
+        image: '/images/sound.jpg',
+        description: 'Description for Product 5',
+        brand: 'Brand 1',
+        category: 'Category 1',
+        price: 32.94,
+        countInStock: 0,
+        rating: 4.8,
+        numReviews: 15
+    },
+
+    {
+        _id: '5',
+        name: 'Sony platstaions 4 pro',
+        image: '/images/playstation.jpg',
+        description: 'Description for Product 5',
+        brand: 'Brand 1',
+        category: 'Category 1',
+        price: 24.99,
+        countInStock: 0,
+        rating: 4.8,
+        numReviews: 15
+    },
+    {
+        _id: '6',
+        name: 'Dell vostro 3401',
+        image: '/images/leptop.jpg',
+        description: 'Description for Product 5',
+        brand: 'Brand 1',
+        category: 'Category 1',
+        price: 45.99,
+        countInStock: 0,
+        rating: 4.8,
+        numReviews: 15
+    },
+
+    {
+        _id: '7',
+        name: ' wireless Headphones',
+        image: '/images/airpods.jpg',
+        description: 'Description for Product 1',
+        brand: 'Brand 1',
+        category: 'Category 1',
+        price: 10.99,
+        countInStock: 5,
+        rating: 4.5,
+        numReviews: 10
+    },
+    {
+        _id: '8',
+        name: 'Echo Dot 3rd Gen',
+        image: '/images/alexa.jpg',
+        description: 'Description for Product 2',
+        brand: 'Brand 2',
+        category: 'Category 2',
+        price: 19.99,
+        countInStock: 2,
+        rating: 3.8,
+        numReviews: 5
+    },
+    {
+        _id: '9',
+        name: 'Sony headphone 4 pro',
+        image: '/images/headphone.jpg',
+        description: 'Description for Product 5',
+        brand: 'Brand 1',
+        category: 'Category 1',
+        price: 21.99,
+        countInStock: 0,
+        rating: 4.8,
+        numReviews: 15
+    },
+
+    {
+        _id: '10',
+        name: 'Sony headphone 4 pro',
+        image: '/images/headphone.jpg',
+        description: 'Description for Product 5',
+        brand: 'Brand 1',
+        category: 'Category 1',
+        price: 21.99,
+        countInStock: 0,
+        rating: 4.8,
+        numReviews: 15
+    },
+
+];
+
+export default products;
