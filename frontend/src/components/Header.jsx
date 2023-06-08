89c54633fa3be4e558231c6ae9b121aa33b13521
Manish <2002mybaran@gmail.com>
2023-06-08 12:53:33 +0530

    frontend authentication complte

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,13 +1,31 @@
-import { Badge,Navbar, Nav, Container } from 'react-bootstrap';
+import {useNavigate} from 'react-router-dom'
+import { Badge, Navbar, Nav, Container, NavDropdown } from 'react-bootstrap';
 import { FaShoppingCart, FaUser } from 'react-icons/fa';
 import { LinkContainer } from 'react-router-bootstrap'
-import {useSelector} from 'react-redux'
+import { useSelector ,useDispatch} from 'react-redux'
+import { useLogoutMutation } from '../slices/usersApiSlice'
+import {logout} from '../slices/authSlice'
 import logo from '../assets/logo.png';
 
 const Header = () => {
-    // how to select somthing from your globel state 
+    // how to select somthing from your globel state
     const { cartItems } = useSelector((state) => state.cart);
-    console.log(cartItems);
+    const { userInfo } = useSelector((state) => state.auth);
+
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const [logoutApiCall] = useLogoutMutation();
+
+    const logoutHandler = async () => {
+        try {
+            await logoutApiCall().unwrap();
+            dispatch(logout()); //just  clearing local stuff
+            navigate('/login');
+        } catch (err) {
+            console.log(err);
+        }
+    }
 
   return (
       <header>
@@ -32,9 +50,20 @@ const Header = () => {
                                   )}
                               </Nav.Link>
                           </LinkContainer>
-                          <LinkContainer to='/login'>
+                          {userInfo ? (
+                              <NavDropdown title={userInfo.name} id='username'>
+                                  <LinkContainer to='/profile'>
+                                      <NavDropdown.Item>Profile</NavDropdown.Item>
+                                  </LinkContainer>
+                                  <NavDropdown.Item onClick={logoutHandler}>
+                                      Logout
+                                  </NavDropdown.Item>
+                          </NavDropdown>
+                          ):(<LinkContainer to = '/login'>
                              <Nav.Link><FaUser />Sign In</Nav.Link>
-                          </LinkContainer>
+                  </LinkContainer>)
+                          }
+                          
                       </Nav>
                   </Navbar.Collapse>
               </Container>
--- frontend/src/slices/usersApiSlice.js
+++ frontend/src/slices/usersApiSlice.js
@@ -5,13 +5,26 @@ export const usersApiSlice = apiSlice.injectEndpoints({
     endpoints: (builder) => ({
         login: builder.mutation({
             query: (data) => ({
-                url: USERS_URL / auth,
+                url: `${USERS_URL}/auth`,
                 method: 'POST',
                 body: data,
             }),
         }),
+        register: builder.mutation({
+            query: (data) => ({
+                url: `${USERS_URL}`,
+                method: 'POST',
+                body: data,
+            })
+        }),
+        logout: builder.mutation({ // for server
+            query: () => ({
+                url: `${USERS_URL}/logout`,
+                method: 'POST',
+            })
+        })
 
     }),
 })
 
-export const { useLoginMutation } = usersApiSlice;
+export const { useLoginMutation, useLogoutMutation, useRegisterMutation } = usersApiSlice;
